e7fee437c91762ebec5c446784ef8d64767e9734
cat-coder <cat-coder>
2022-04-30 18:53:23 -0500

    swithc actions

--- src/store/reducers/Switch.js
+++ src/store/reducers/Switch.js
@@ -1,25 +1,24 @@
-import { createSlice,  } from "@reduxjs/toolkit";
-
+import * as actions from "../actions";
 
 const initialSwitchState = { isSwitch: false };
 
 
-const switchSlice = createSlice({
-    name: 'switch', 
-    initialState: initialSwitchState,
-   
-    reducers: {
-        weatherPage(state){
-            state.isSwitch = true;
-        },
-        nowWhat(state){
-            state.isSwitch = false;
-        },
+const weatherPage =(state, action)=>{
+    return {...state, error: action.error}
+}
 
-    }
-});
-
-export const switchActions = switchSlice.actions;
+const nowWhat =(state, action)=>{
+    return {...state, error: action.error}
+}
 
 
-export default switchSlice.reducer
+const handlers = {
+    [actions.SWITCH_WEATHER]: weatherPage
+    [actions.SWITCH_NOWWHAT]: nowWhat
+  };
+
+export default (state = initialState, action)=>{
+    const handler = handlers[action.type];
+    if (typeof handler === 'undefined') return state;
+    return handler(state, action);
+}
