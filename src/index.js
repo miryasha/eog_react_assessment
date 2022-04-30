f6688b57ca69052f51349ecae01978bb539da79e
cat-coder <cat-coder>
2022-04-30 11:56:20 -0500

    subscription and urql added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider, createClient, subscriptionExchange, defaultExchanges} from 'urql'
+import {SubscriptionClient} from 'subscriptions-transport-ws';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const subscriptionClient = new SubscriptionClient('ws://react.eogresources.com/graphql')
+
+const client = createClient({
+    url: "https://react-assessment.herokuapp.com/graphql",
+    exchanges: [...defaultExchanges, subscriptionExchange({
+        forwardSubscription: operation =>
+        subscriptionClient.request(operation)
+    })]
+  });
+
+  ReactDOM.render(
+    <Provider value = {client}>
+        <App />
+    </Provider>
+, document.getElementById("root")
+);
